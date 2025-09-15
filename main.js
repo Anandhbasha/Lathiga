91eeef6ed45c30b4c0fd866745d3c84f3a92f5d7
Anandhbasha <anadhakm@gmail.com>
2025-09-15 10:04:28 +0530

    js intro

---
+++ main.js
@@ -0,0 +1,77 @@
+// Hositing
+    // var 
+    // console.log(a)
+    // var a =10  
+    // var a=20
+    // a=60
+    // console.log(a);    
+        // = assignment operator
+    //let
+    // console.log(b);    
+    // let b=55
+    // b=70
+    // console.log(b);
+    //const
+    // console.log(d);
+    
+    // const d=22
+    // d=66
+    // console.log(d);
+
+    // localscope
+    // globalscope
+    // {
+        // const num1 = 55
+    // }
+    // console.log(num1);
+
+    // starts with letter
+    // - dont use
+    // space don't use
+    //keyword don't use
+
+// data types
+    //premitive
+        // number
+        let num2 = 50
+        console.log(typeof(num2));
+        
+        // string
+        let user = 'xyz'
+        console.log(typeof(user));
+        
+        // boolean
+        let isComing = true
+        console.log(typeof(isComing));
+        
+        // undefined
+        let x = undefined
+        console.log(typeof(x));
+        
+        // null
+        let y = null
+        console.log(typeof(y));
+// non premitive
+    // array
+        let arr = [50,'xyz',true,undefined,null] //0,1,2,3,4
+        console.log(arr);
+        console.log(arr[0]);
+        console.log(arr[3]);       
+        
+    // object
+    let person = {
+        personName:"abc",
+        personAge:22,
+        personCity:"CBE",
+        family:{
+            dad:"ghjgghj",
+            mom:"hggjkjhkjh",
+            siblings:{
+                brother:"bhghhjk",
+                sister:"jhghkjnnh"
+            }
+        }
+    }
+    console.log(person.family.siblings.brother);
+    
+    
