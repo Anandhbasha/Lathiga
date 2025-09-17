088380e21d0865d3a40dc4e33430560af36bfc87
Anandhbasha <anadhakm@gmail.com>
2025-09-17 10:16:59 +0530

    loops

--- main.js
+++ main.js
@@ -15,13 +15,13 @@
     // console.log(d);
     
     // const d=22
-    // d=66
+    // // d=66
     // console.log(d);
 
     // localscope
     // globalscope
     // {
-        // const num1 = 55
+    //     const num1 = 55
     // }
     // console.log(num1);
 
@@ -29,7 +29,6 @@
     // - dont use
     // space don't use
     //keyword don't use
-
 // data types
     //premitive
         // number
@@ -45,12 +44,12 @@
 //         console.log(typeof(isComing));
         
 //         // undefined
-//         let x = undefined
-//         console.log(typeof(x));
+        // let x =undefined
+        // console.log(typeof(x));
         
 //         // null
-//         let y = null
-//         console.log(typeof(y));
+        // let y = null
+        // console.log(typeof(y));
 // // non premitive
 //     // array
 //         let arr = [50,'xyz',true,undefined,null] //0,1,2,3,4
@@ -107,15 +106,15 @@
     //     console.log(age>19?"adult":"teenager");
         
     // functions
-    function add(a,b){
-        return a+b;        
-    }
-    // add(50,60)
-    // add(66,77)
-    // add(66,75)
-    // add(66,66)
+    // function add(a,b){
+    //     return a+b;        
+    // }
+    // // add(50,60)
+    // // add(66,77)
+    // // add(66,75)
+    // // add(66,66)
 
-    console.log(add(97,68));
+    // console.log(add(97,68));
     // let age =19
     // statements
         // conditional Statements
@@ -131,27 +130,104 @@
                 
             // }
 
-            let age =19
+            // let age =19
 
-            // else if
-            if(age>19){
-                console.log("He is Adult");                
-            }
-            else if(age>=13 && age<=19){
-                console.log("He is Teenager");
+            // // else if
+            // if(age>19){
+            //     console.log("He is Adult");                
+            // }
+            // else if(age>=13 && age<=19){
+            //     console.log("He is Teenager");
                 
-            }
-            else{
-                console.log("He is Child");
+            // }
+            // else{
+            //     console.log("He is Child");
                 
-            }
-            nationality = "India"
-            state = "TN"
-            // nested if
-            if(age>=18){
-                if(nationality=="India"){
-                    if(state=="TN"){
-                        console.log("He is eligible to vote in TN");                        
-                    }
-                }
-            }
+            // }
+            // nationality = "India"
+            // state = "TN"
+            // // nested if
+            // if(age>=18){
+            //     if(nationality=="India"){
+            //         if(state=="TN"){
+            //             console.log("He is eligible to vote in TN");                        
+            //         }
+            //     }
+            // }
+
+// switch
+let today="Wednesday"
+switch(today){
+    case "Monday":
+        console.log("Today is Monday");
+        break
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break
+    case "Thursday":
+        console.log("Today is Thursday");
+        break
+    case "Friday":
+        console.log("Today is Friday");
+        break
+    case "Saturday":
+        console.log("Today is Saturday");
+        break
+    default:
+        console.log("Today is Sunday");
+        
+}
+// loops
+    //while
+    let num1 = 20
+    while(num1<25){
+        console.log("While is working");
+        num1++        
+    }
+
+    // let arr = [10,20,55,44,88]
+    // console.log(arr[0]);
+    // console.log(arr[1]);
+    // console.log(arr[2]);
+    // console.log(arr[3]);
+    // console.log(arr[4]);
+    // let x=0
+    // while(x<arr.length){
+    //     console.log(arr[x]); //arr[0] ,x=0
+    //     x++
+    // }
+    
+    // //do while
+    // do{
+    //     console.log("Do while is working");        
+    // }while(10>15)
+    // // for
+    // for(let x=0;x<arr.length;x++){
+    //     console.log(arr[x]);
+    // }
+    // for(let x=arr.length-1;x>=0;x--){
+    //     console.log(arr[x]);
+    // }
+
+    // let names = "Giri"
+    // let temp=""
+    // for(let x =0;x<names.length;x++){
+    //     temp = temp+names[x] //G
+    //     console.log(temp);
+        
+    // }
+    // for(let x =names.length-1;x>=0;x--){
+    //     temp = temp+names[x] //G
+    //     console.log(temp);
+        
+    // }
+     let arr = [10,20,55,44,88]
+    let total = 0
+    for(let x=0;x<arr.length;x++){
+        total+=arr[x]        
+    }
+    console.log(total);
+    
