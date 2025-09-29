02aecadcecab61482ab03e78be35f1e29504efbf
Anandhbasha <anadhakm@gmail.com>
2025-09-29 10:36:27 +0530

    java

---
+++ Java/datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes {
+    public static void main(String[] args){
+        // premitive data types
+        // byte a =10;
+        // short b = 20;
+        // int x = 50;
+        // long f = 22;
+        // float d = 50.6f;
+        // double h = 600000d;
+        // char j = 'A';
+        // non premetive
+        String name = "Hello";
+        int [] arr = {10,20,30,40};
+        // class or Object
+    }
+}
---
+++ Java/statements.java
@@ -0,0 +1,10 @@
+public class statements {
+    public static void main(String[] args) {
+        // if
+        int a = 10;
+        int b = 5;
+        if(a>b){
+            System.out.println("If is Working");
+        }
+    }
+}
