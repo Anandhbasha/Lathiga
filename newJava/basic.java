8a470f2f9d6aa7782e3de9df67c0cec2520daa5b
Anandhbasha <anadhakm@gmail.com>
2025-11-06 10:20:35 +0530

    datatypes

---
+++ newJava/basic.java
@@ -0,0 +1,19 @@
+public class basic {
+    public static void main(String[] args) {
+        // datatyoes
+        // integer
+        int a = 10;
+        // float
+        float b = 20;
+        // double
+        double x = 20.50;
+        byte d= 12;
+        long f = 5000;
+        short g = 2;
+        char o = 'j';
+        String names = "Lathiga";
+        int [] arr = {20,40,50,60};
+        System.out.println(a);
+    }
+
+}
